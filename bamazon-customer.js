487f3456d6330b86da671fae73ef17805932a061
Angelo Bovino <angelo.bovino@outlook.com>
2016-07-23 18:22:02 -0400

    adds function to display all products in bamazondb

--- bamazon-customer.js
+++ bamazon-customer.js
@@ -7,7 +7,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "olejuve123",
     database: "bamazondb"
 })
 
@@ -21,4 +21,10 @@ connection.connect(function(err) {
 //Displays all products in the database
 var start = function() {
   console.log("start");
+  var query = 'SELECT * FROM Products';
+  connection.query(query, function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log("Item #" + res[i].ItemID + " | " + res[i].ProductName + " | " + res[i].DepartmentName + " | " + res[i].Price + " | " + res[i].StockQuantity + "\n");
+    }
+  })
 }
