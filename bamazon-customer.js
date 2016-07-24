a1ac791cb66c9a5e5e8865d80e98ade9111e9163
Angelo Bovino <angelo.bovino@outlook.com>
2016-07-24 17:09:56 -0400

    Adds function to create product table in CLI

--- bamazon-customer.js
+++ bamazon-customer.js
@@ -1,13 +1,14 @@
 //Declares Dependencies
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+var Table = require('cli-table2');
 
 //Connects to the MySQL localhost
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "olejuve123",
     database: "bamazondb"
 })
 
@@ -18,13 +19,28 @@ connection.connect(function(err) {
     start();
 })
 
+var productTable = new Table({
+  head: ['ID#', 'Product Name', 'Department', 'Price', 'Stock'],
+  colWidths: [4, 50, 15, 10, 10],
+  wordWrap:true
+});
+
 //Displays all products in the database
 var start = function() {
-  console.log("start");
+  console.log(" -------------------------------");
+  console.log("* Welcome to Bamazon CMD Store! *");
+  console.log(" -------------------------------\n");
   var query = 'SELECT * FROM Products';
   connection.query(query, function(err, res) {
+    
     for (var i = 0; i < res.length; i++) {
-      console.log("Item #" + res[i].ItemID + " | " + res[i].ProductName + " | " + res[i].DepartmentName + " | " + res[i].Price + " | " + res[i].StockQuantity + "\n");
+      productTable.push(
+        [res[i].ItemID, res[i].ProductName, res[i].DepartmentName, "$" + res[i].Price, res[i].StockQuantity]
+      );
+      /*
+      console.log("Item ID Number: " + res[i].ItemID + " | " + res[i].ProductName + " | " + res[i].DepartmentName + " | " + res[i].Price + " | " + res[i].StockQuantity + "\n");*/
     }
+    console.log(productTable.toString());
   })
+
 }
